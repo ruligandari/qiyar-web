49b81e29760af84864fb874b05d11d074737ed7c
ruligandari <ruligandari@gmail.com>
2023-09-08 21:03:20 +0700

    fix tampilan login

--- app/Views/login/index.php
+++ app/Views/login/index.php
@@ -119,15 +119,15 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url('/') ?>vendor/jquery/jquery.min.js"></script>
+  <script src="<?= base_url('/') ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="<?= base_url('/') ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
-  <script src="sweetalert2.all.min.js"></script>
+  <script src="<?= base_url('/') ?>js/sb-admin-2.min.js"></script>
+  <script src="<?= base_url('/') ?>sweetalert2.all.min.js"></script>
 </body>
 
 </html>
