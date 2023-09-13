5cd80b9d9ec8de65457e44535ebdc4272092da1d
ruligandari <ruligandari@gmail.com>
2023-09-13 15:41:41 +0700

    fix vendor to plugin

--- app/Views/template/template.php
+++ app/Views/template/template.php
@@ -13,7 +13,7 @@
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('/') ?>css/custom-datatable.css" rel="stylesheet">
-    <link href="<?= base_url('/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('/') ?>plugin/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
@@ -295,17 +295,17 @@
 
     <?= $this->renderSection('custom-js'); ?>
     <!-- Bootstrap core JavaScript-->
-    <script src="<?= base_url() ?>/vendor/jquery/jquery.min.js"></script>
-    <script src="<?= base_url() ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url() ?>/plugin/jquery/jquery.min.js"></script>
+    <script src="<?= base_url() ?>/plugin/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?= base_url() ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?= base_url() ?>/plugin/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="<?= base_url() ?>/js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="<?= base_url() ?>/vendor/chart.js/Chart.min.js"></script>
+    <script src="<?= base_url() ?>/plugin/chart.js/Chart.min.js"></script>
 
     <!-- Page level custom scripts -->
     <script src="<?= base_url() ?>/js/demo/chart-area-demo.js"></script>
