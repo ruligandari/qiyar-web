440ebe6286900c956a3af3f4a97e6b61737c9047
ruligandari <ruligandari@gmail.com>
2023-09-21 14:00:23 +0700

    fix tampilan warehouse untuk user selain 2367

--- app/Views/dashboard/warehouse/kuningan/warehouse.php
+++ app/Views/dashboard/warehouse/kuningan/warehouse.php
@@ -208,81 +208,406 @@
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <script>
-    <?php if (in_array(session()->get('role'), ['2', '3', '6'])) : ?>
-        var columnBarangMasukKng = [{
-            data: 'no',
-            orderable: false
-        }, {
-            data: 'tanggal'
-        }, {
-            data: 'nama_barang'
-        }, {
-            data: 'qty'
-        }, {
-            data: 'action'
-        }, ];
-    <?php else : ?>
-        var columnBarangMasukKng = [{
-            data: 'no',
-            orderable: false
-        }, {
-            data: 'tanggal'
-        }, {
-            data: 'nama_barang'
-        }, {
-            data: 'qty'
-        }];
-    <?php endif; ?>
+    $(function() {
 
-    <?php if (in_array(session()->get('role'), ['2', '3', '6'])) : ?>
-        var columnBarangKeluarKng = [{
-                data: 'no',
+        var start = moment().subtract(29, 'days');
+        var end = moment();
+
+        function cb(start, end) {
+            $('input[name="datesBarangMasuk"]').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        }
+
+        $('#datesBarangMasuk').daterangepicker({
+            startDate: start,
+            endDate: end,
+            ranges: {
+                'Semua': [moment().subtract(1, 'years'), moment()],
+                'Hari Ini': [moment(), moment()],
+                'Kemarin': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                '7 Hari Terakhir': [moment().subtract(6, 'days'), moment()],
+                '30 Hari Terakhir': [moment().subtract(29, 'days'), moment()],
+                'Bulan Ini': [moment().startOf('month'), moment().endOf('month')],
+                'Bulan Lalu': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            }
+        }, cb);
+
+        cb(start, end);
+
+    });
+
+    var urlTable = $('#urlBarangMasuk').val();
+    var urlDelete = $('#urlDelete').val();
+    console.log(urlDelete);
+    console.log(urlTable);
+
+    $(document).ready(function() {
+        let table = $('#table1').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: urlTable,
+                method: 'POST',
+                data: function(d) {
+                    d.dates = $('input[name="datesBarangMasuk"]').val();
+                },
+            },
+            dom: '<"button-container"lBfrtip>',
+            buttons: [{
+                    extend: 'excelHtml5',
+                    footer: true,
+                    title: 'Data Barang Masuk - ' + formattedDate,
+                    exportOptions: {
+                        columns: [0, 1, 2, 3],
+                    },
+                    className: 'mb-2',
+                    // ubah nama file ketika di download
+
+                },
+                {
+                    extend: 'pdfHtml5',
+                    footer: true,
+                    title: 'Data Barang Masuk - ' + formattedDate,
+                    exportOptions: {
+                        columns: [0, 1, 2, 3],
+                    },
+                    className: 'mb-2',
+                }
+            ],
+            columns: [{
+                    data: 'no',
+                    orderable: false
+                }, {
+                    data: 'tanggal'
+                }, {
+                    data: 'nama_barang'
+                }, {
+                    data: 'qty'
+                },
+                <?php if (in_array(session()->get('role'), ['2', '3', '6'])) : ?> {
+                        data: 'action'
+                    }
+                <?php endif ?>
+            ],
+            lengthMenu: [
+                [10, 25, 50, -1],
+                [10, 25, 50, 'Semua']
+            ], // Pilihan jumlah data per halaman, termasuk "Semua"
+            pageLength: 10,
+            order: [],
+            columnDefs: [{
+                targets: -1,
                 orderable: false
+            }, ],
+            initComplete: function() {
+                // Fungsi untuk menghitung jumlah total kolom "jumlah"
+                function sumColumn() {
+                    let columnData = table.column(3, {
+                        search: 'applied'
+                    }).data();
+
+                    // Check if columnData is defined and not empty
+                    if (columnData && columnData.length > 0) {
+                        let sum = columnData.reduce(function(acc, curr) {
+                            let numericValue = parseFloat(curr.replace(/\./g, '').replace(',', '.')); // Parse angka
+                            return acc + numericValue;
+                        }, 0);
+
+                        // Format jumlah total sebagai mata uang Indonesia
+                        let formattedSum = sum.toLocaleString('id-ID', {
+                            minimumFractionDigits: 0, // Atur ini ke 0 untuk menghilangkan angka di belakang koma
+                            maximumFractionDigits: 2
+                        });
+
+                        // Tampilkan jumlah total di elemen HTML dengan ID "totalSum"
+                        $('#totalSum').html(formattedSum);
+                    } else {
+                        // If columnData is undefined or empty, display a default value
+                        $('#totalSum').html('0');
+                    }
+                }
+
+
+                // Panggil fungsi sumColumn saat tabel selesai dimuat
+                sumColumn();
+
+                // Panggil fungsi sumColumn lagi ketika tabel di-filter atau di-sort
+                table.on('search.dt draw.dt', sumColumn);
             },
-            {
-                data: 'tanggal'
-            },
-            {
-                data: 'nama_barang'
-            },
-            {
-                data: 'qty'
-            },
-            {
-                data: 'total_resi'
-            },
-            {
-                data: 'bukti_pickup'
-            }, {
-                data: 'action'
-            },
-        ];
-    <?php else : ?>
-        var columnBarangKeluarKng = [{
-                data: 'no',
-                orderable: false
-            },
-            {
-                data: 'tanggal'
-            },
-            {
-                data: 'nama_barang'
-            },
-            {
-                data: 'qty'
-            },
-            {
-                data: 'total_resi'
-            },
-            {
-                data: 'bukti_pickup'
-            },
-        ];
-    <?php endif; ?>
+        });
+
+        $('#datesBarangMasuk').change(function(event) {
+            table.ajax.reload();
+        });
+
+    });
+
+    let currentDate = new Date();
+    let formattedDate = currentDate.toISOString().split('T')[0];
+
+    function deleteKngMasuk(id) {
+        Swal.fire({
+            title: "Apakah Anda yakin akan menghapus data ini?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Ya, hapus!",
+            cancelButtonText: "Tidak, batalkan!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Kirim permintaan hapus menggunakan Ajax
+                $.ajax({
+                    method: "POST",
+                    url: urlDelete,
+                    data: {
+                        id: id
+                    },
+                    success: function(response) {
+                        var data = JSON.parse(response);
+                        if (data.success) {
+                            Swal.fire(
+                                "Dihapus!",
+                                "Data Berhasil Dihapus",
+                                "success"
+                            ).then(() => {
+                                // Muat ulang halaman setelah penghapusan
+                                location.reload();
+                            });
+                        } else {
+                            Swal.fire(
+                                "Error!",
+                                "Gagal menghapus data.",
+                                "error"
+                            );
+                        }
+                    },
+                    error: function() {
+                        Swal.fire(
+                            "Error!",
+                            "Terjadi kesalahan saat menghapus data.",
+                            "error"
+                        );
+                    },
+                });
+            }
+        });
+    }
 </script>
+<script>
+    $(function() {
 
-<script src="<?= base_url('js/data-table-barang-masuk-kng.js') ?>"></script>
-<script src="<?= base_url('js/data-table-barang-keluar-kng.js') ?>"></script>
+        var start = moment().subtract(29, 'days');
+        var end = moment();
+
+        function cb(start, end) {
+            $('input[name="datesBarangKeluar"]').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        }
+
+        $('#datesBarangKeluar').daterangepicker({
+            startDate: start,
+            endDate: end,
+            ranges: {
+                'Semua': [moment().subtract(1, 'years'), moment()],
+                'Hari Ini': [moment(), moment()],
+                'Kemarin': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                '7 Hari Terakhir': [moment().subtract(6, 'days'), moment()],
+                '30 Hari Terakhir': [moment().subtract(29, 'days'), moment()],
+                'Bulan Ini': [moment().startOf('month'), moment().endOf('month')],
+                'Bulan Lalu': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            }
+        }, cb);
+
+        cb(start, end);
+
+    });
+
+    var urlTableKeluar = $('#urlBarangKeluar').val();
+    var urlDeleteKeluar = $('#urlDeleteBarangKeluar').val();
+
+    console.log(urlDeleteKeluar);
+    console.log(urlTableKeluar);
+
+    $(document).ready(function() {
+        let table = $('#table2').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: urlTableKeluar,
+                method: 'POST',
+                data: function(d) {
+                    d.dates = $('input[name="datesBarangKeluar"]').val();
+                },
+            },
+            dom: '<"button-container"lBfrtip>',
+            buttons: [{
+                    extend: 'excelHtml5',
+                    footer: true,
+                    title: 'Data Barang Keluar - ' + formattedDate,
+                    exportOptions: {
+                        columns: [0, 1, 2, 3, 4, 5],
+                        format: {
+                            body: function(data, row, column, node) {
+                                // Jika kolom adalah gambar, return elemen img
+                                if (column === 5) {
+                                    return $('img', data).attr('src');
+                                }
+                                return data;
+                            }
+                        }
+                    },
+                    className: 'mb-2',
+                    // ubah nama file ketika di download
+
+                },
+                {
+                    extend: 'pdfHtml5',
+                    footer: true,
+                    title: 'Data Barang Keluar - ' + formattedDate,
+                    exportOptions: {
+                        columns: [0, 1, 2, 3, 4, 5],
+                        format: {
+                            body: function(data, row, column, node) {
+                                // Jika kolom adalah gambar, return elemen img
+                                if (column === 5) {
+                                    return $('img', data).attr('src');
+                                }
+                                return data;
+                            }
+                        }
+                    },
+                    className: 'mb-2',
+                }
+            ],
+            columns: [{
+                    data: 'no',
+                    orderable: false
+                },
+                {
+                    data: 'tanggal'
+                },
+                {
+                    data: 'nama_barang'
+                },
+                {
+                    data: 'qty'
+                },
+                {
+                    data: 'total_resi'
+                },
+                {
+                    data: 'bukti_pickup'
+                },
+                <?php if (in_array(session()->get('role'), ['2', '3', '6'])) : ?> {
+                        data: 'action'
+                    },
+                <?php endif ?>
+            ],
+            lengthMenu: [
+                [10, 25, 50, -1],
+                [10, 25, 50, 'Semua']
+            ], // Pilihan jumlah data per halaman, termasuk "Semua"
+            pageLength: 10,
+            order: [],
+            columnDefs: [{
+                targets: -1,
+                orderable: false
+            }, ],
+            initComplete: function() {
+                // Fungsi untuk menghitung jumlah total kolom "jumlah"
+                function sumColumn() {
+                    let sum = table.column(3, {
+                        search: 'applied'
+                    }).data().reduce(function(acc, curr) {
+                        let numericValue = parseFloat(curr.replace(/\./g, '').replace(',', '.')); // Parse angka
+                        return acc + numericValue;
+                    }, 0);
+                    let sumResi = table.column(4, {
+                        search: 'applied'
+                    }).data().reduce(function(acc, curr) {
+                        let numericValue = parseFloat(curr.replace(/\./g, '').replace(',', '.')); // Parse angka
+                        return acc + numericValue;
+                    }, 0);
+
+                    // Format jumlah total sebagai mata uang Indonesia
+                    let formattedSum = sum.toLocaleString('id-ID', {
+                        minimumFractionDigits: 0, // Atur ini ke 0 untuk menghilangkan angka di belakang koma
+                        maximumFractionDigits: 2
+                    });
+                    let formattedSumResi = sumResi.toLocaleString('id-ID', {
+                        minimumFractionDigits: 0, // Atur ini ke 0 untuk menghilangkan angka di belakang koma
+                        maximumFractionDigits: 2
+                    });
+
+                    // Tampilkan jumlah total di elemen HTML dengan ID "totalSum"
+                    $('#totalQty').html(formattedSum);
+                    $('#totalResi').html(formattedSumResi);
+                }
+
+                // Panggil fungsi sumColumn saat tabel selesai dimuat
+                sumColumn();
+
+                // Panggil fungsi sumColumn lagi ketika tabel di-filter atau di-sort
+                table.on('search.dt draw.dt', sumColumn);
+            },
+        });
+
+        $('#datesBarangKeluar').change(function(event) {
+            table.ajax.reload();
+        });
+
+    });
+
+
+    function deleteKngKeluar(id) {
+        Swal.fire({
+            title: "Apakah Anda yakin akan menghapus data ini?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Ya, hapus!",
+            cancelButtonText: "Tidak, batalkan!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Kirim permintaan hapus menggunakan Ajax
+                $.ajax({
+                    type: "POST",
+                    url: urlDeleteKeluar,
+                    data: {
+                        id: id
+                    },
+                    success: function(response) {
+                        var data = JSON.parse(response);
+                        console.log(data);
+                        if (data.success) {
+                            Swal.fire(
+                                "Dihapus!",
+                                "Data Berhasil Dihapus",
+                                "success"
+                            ).then(() => {
+                                // Muat ulang halaman setelah penghapusan
+                                location.reload();
+                            });
+                        } else {
+                            Swal.fire(
+                                "Error!",
+                                "Gagal menghapus data.",
+                                "error"
+                            );
+                        }
+                    },
+                    error: function() {
+                        Swal.fire(
+                            "Error!",
+                            "Terjadi kesalahan saat menghapus data.",
+                            "error"
+                        );
+                    },
+                });
+            }
+        });
+    }
+</script>
 <script src="<?= base_url('js/sweet-alert.js') ?>"></script>
 
 <?= $this->endsection(); ?>
